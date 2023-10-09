3af0405da4dd34a89c5e37256c5fc647f63c4376
Banashbib <Banashbib123@gmail.com>
2023-10-09 17:44:52 +0300

    update on welcoming pages

---
+++ Flosna/Welcome2.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome2.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Welcome2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome2()
+}
