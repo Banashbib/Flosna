67b8313e22aa4d109afa98a839101d19661069a1
ahlam10 <ahlam_11a@hotmail.com>
2023-10-09 16:24:34 +0300

    ولد اشتغل

--- Flosna/الاسئلة الشائعة.swift
+++ Flosna/الاسئلة الشائعة.swift
@@ -5,4 +5,13 @@
 //  Created by Ahlam ALshehri on 24/03/1445 AH.
 //
 
+import SwiftUI
 import Foundation
+struct الاسئلةالشائعة: View {
+    var body: some View {
+        VStack {
+            Text("Leen")
+        }
+        .padding()
+    }
+}
