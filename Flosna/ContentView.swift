21ddd4b61f4dc76b3dbacb7cac5ad91f283ea38b
Banashbib <Banashbib123@gmail.com>
2023-10-09 15:24:29 +0300

    Initial Commit

---
+++ Flosna/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flosna/FlosnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlosnaApp.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FlosnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
