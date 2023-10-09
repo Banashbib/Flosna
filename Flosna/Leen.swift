5b1e68eb2f3e81c8c6c7fdc6f2d2bf66693f2f96
Leen73XX <leen.mej@gmail.com>
2023-10-09 16:11:35 +0300

    Leen file add

---
+++ Flosna/Leen.swift
@@ -0,0 +1,16 @@
+//
+//  Leen.swift
+//  Flosna
+//
+//  Created by Leen Almejarri on 24/03/1445 AH.
+//
+import SwiftUI
+import Foundation
+struct Leen: View {
+    var body: some View {
+        VStack {
+            Text("Leen")
+        }
+        .padding()
+    }
+}
