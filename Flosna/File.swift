46857a0baa7fb573e0d146ae643a067c3c48ae74
Banashbib <Banashbib123@gmail.com>
2023-10-09 16:55:05 +0300

    Welcome page update

---
+++ Flosna/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 24/03/1445 AH.
+//
+
+import Foundation
---
+++ Flosna/welcom1.swift
@@ -0,0 +1,38 @@
+//
+//  welcom1.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct welcom1: View {
+    var body: some View {
+        ZStack (alignment: .topTrailing){
+            Image("welcom page 1")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                            
+                            print("Skip button tapped")
+                        }) {
+                            Text("تخطي")
+                                .font(.headline)
+                                .padding(10)
+                                //.background(Color.gray)
+                                .foregroundColor(.gray)
+                                .cornerRadius(8)
+                                .padding(.trailing, 20)
+                                .padding(.top, 3)
+                        }
+
+            
+            
+        }
+    }
+}
+
+#Preview {
+    welcom1()
+}
