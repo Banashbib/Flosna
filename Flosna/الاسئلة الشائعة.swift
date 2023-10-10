0df2ff529192e0895a1be1750572076ad1126e27
Banashbib <Banashbib123@gmail.com>
2023-10-10 15:16:39 +0300

    simple update

--- Flosna/الاسئلة الشائعة.swift
+++ Flosna/الاسئلة الشائعة.swift
@@ -10,7 +10,7 @@ import Foundation
 struct الاسئلةالشائعة: View {
     var body: some View {
         VStack {
-            Text("Leen")
+            Text("Leen    ")
         }
         .padding()
     }
