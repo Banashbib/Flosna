16b7c9d92e5f50241f543e87d9c1ecc7a93b7ad5
Leen73XX <leen.mej@gmail.com>
2023-10-10 17:35:18 +0300

    add log out and delet account

--- Flosna/الملف الشخصي.swift
+++ Flosna/الملف الشخصي.swift
@@ -7,20 +7,23 @@
 import SwiftUI
 import Foundation
 
-struct Leen: View {
+struct الملفالشخصي: View {
     @State var tog : Bool = false
+    @State private var showLogoutAlert = false
+    @State private var showDeleteAccountAlert = false
     var body: some View {
         VStack{
-        NavigationView {
-            
-            VStack{
+            NavigationView {
+                
+                VStack (spacing:30){
+                    
                     Divider()
-                        .padding()
-                    Image("sara").resizable().frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/).cornerRadius(400)
+                       
+                    Image("sara").resizable().frame(width:200 , height:200).cornerRadius(400)
                         .padding()
                     VStack(){
                         ZStack{
-                          Rectangle()
+                            Rectangle()
                                 .fill(Color.ourgray).frame(width: 346.0, height: 54.8)
                                 .cornerRadius(9)
                             
@@ -34,10 +37,10 @@ struct Leen: View {
                         //Vstack cont.
                         ZStack{
                             Rectangle()
-                                  .fill(Color.ourgray).frame(width: 346.0, height: 54.8)
-                                  .cornerRadius(9)
-                              
-                            .padding()
+                                .fill(Color.ourgray).frame(width: 346.0, height: 54.8)
+                                .cornerRadius(9)
+                            
+                                .padding()
                             Text("اللغة")
                                 .font(.title)
                                 .multilineTextAlignment(.trailing)
@@ -47,26 +50,83 @@ struct Leen: View {
                         //Vstack cont.
                         ZStack{
                             Rectangle()
-                                  .fill(Color.ourgray).frame(width: 346.0, height: 54.8)
-                                  .cornerRadius(9)
-                              
+                                .fill(Color.ourgray).frame(width: 346.0, height: 54.8)
+                                .cornerRadius(9)
+                            
                             
                             Text("الاسئلة الشائعة")
                                 .font(.title)
                                 .multilineTextAlignment(.trailing)
                                 .padding(.leading, 180)
-                        } //end Zstack الاسئلة الشائعة
+                            //end Zstack
+                        }
                         
+                        //Vstack cont. تسجيل خروج
+                        HStack{
+                            VStack {
+                                Button(action: {
+                                    showLogoutAlert = true
+                                }) {
+                                    Text("تسجيل الخروج")
+                                        .foregroundColor(.white)
+                                        .font(.headline)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .background(Color.ourgreen)
+                                        .cornerRadius(10)
+                                }
+                                .alert(isPresented: $showLogoutAlert) {
+                                    Alert(
+                                        title: Text("تأكيد تسجيل الخروج"),
+                                        message: Text("هل أنت متأكد من رغبتك في تسجيل الخروج؟"),
+                                        primaryButton: .cancel(Text("إلغاء")),
+                                        secondaryButton: .destructive(Text("تسجيل الخروج"), action: {
+                                            // تنفيذ الإجراء المرتبط بتسجيل الخروج هنا
+                                        })
+                                    )
+                                }
+                                
+                                
+                            }
+                            Button(action: {
+                                showDeleteAccountAlert = true
+                            }) {
+                                Text("حذف الحساب")
+                                    .foregroundColor(Color.ourgreen)
+                                    .font(.headline)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.white)
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10).stroke(Color.ourgreen , lineWidth:1))
+                            }
+                            .alert(isPresented: $showDeleteAccountAlert) {
+                                Alert(
+                                    title: Text("تأكيد حذف الحساب"),
+                                    message: Text("هل أنت متأكد من رغبتك في حذف الحساب؟"),
+                                    primaryButton: .cancel(Text("إلغاء")),
+                                    secondaryButton: .destructive(Text("حذف الحساب"), action: {
+                                        // تنفيذ الإجراء المرتبط بحذف الحساب هنا
+                                    })
+                                )
+                            }
+                        }
+                        
+                    }
                 }
-                    Spacer()
-                         .navigationBarTitle("الملف الشخصي")
-
-            
+                
+                
+                    .navigationBarTitle("الملف الشخصي")
+                
+                
             }
-        }}
             .padding()
-        
-    }}
+        }}}
+
+          
+            
+
 #Preview {
-    Leen()
+    الملفالشخصي()
 }
