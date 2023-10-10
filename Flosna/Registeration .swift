344a010fcb2b92affbdf5a054141587052ae95e0
Banashbib <Banashbib123@gmail.com>
2023-10-10 15:33:17 +0300

    addding the registaration page

---
+++ Flosna/Registeration .swift
@@ -0,0 +1,18 @@
+//
+//  Registeration .swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Registeration_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Registeration_()
+}
