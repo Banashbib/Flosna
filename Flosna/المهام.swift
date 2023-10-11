e016f3150f2e9ac9a8c6bc85ea092a7f42d1de75
ahlam10 <ahlam_11a@hotmail.com>
2023-10-11 15:02:50 +0300

    المهام

---
+++ Flosna/المهام.swift
@@ -0,0 +1,18 @@
+//
+//  المهام.swift
+//  Flosna
+//
+//  Created by Ahlam ALshehri on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ______: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ______()
+}
