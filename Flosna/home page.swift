acf15d9bf61136c7530c2dea98affcbda6aacb9a
Leen73XX <leen.mej@gmail.com>
2023-10-14 17:06:40 +0300

    home page

---
+++ Flosna/home page.swift
@@ -0,0 +1,18 @@
+//
+//  home page.swift
+//  Flosna
+//
+//  Created by Leen Almejarri on 29/03/1445 AH.
+//
+
+import SwiftUI
+
+struct home_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    home_page()
+}
