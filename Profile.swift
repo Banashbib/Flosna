c3d2e69fdd944dcd9d7e61e7c726616b04861040
Banashbib <Banashbib123@gmail.com>
2023-10-15 16:13:11 +0300

    updattttteeee!!!

---
+++ Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  Flosna
+//
+//  Created by Bana Alshabib on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
