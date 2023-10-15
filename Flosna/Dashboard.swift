3d2766203f3c07157b11f2ae641b7f4053c33dd0
rahafsaad <rahafsaad8@gmail.com>
2023-10-15 16:20:30 +0300

    adding splash page + dashboard page

---
+++ Flosna/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  Flosna
+//
+//  Created by Rahaf on 15/10/2023.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
---
+++ Flosna/splash page.swift
@@ -0,0 +1,18 @@
+//
+//  splash page.swift
+//  Flosna
+//
+//  Created by Rahaf on 15/10/2023.
+//
+
+import SwiftUI
+
+struct splash_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    splash_page()
+}
