076d33f4c9d2f2d0a073af0b9b42a99f1492669a
hajar <hajarshehri92@gmail.com>
2023-10-15 15:29:40 +0300

    plan page last update

---
+++ Flosna/plan.swift
@@ -0,0 +1,18 @@
+//
+//  plan.swift
+//  Flosna
+//
+//  Created by Hajar Alshehri on 27/03/1445 AH.
+//
+
+import SwiftUI
+
+struct plan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    plan()
+}
